5e748797aa655570f53eeae44fff0ce6dcb5288c
Tomas Achaval <134091945+achaval-tomas@users.noreply.github.com>
2023-10-16 13:47:31 -0300

    Time: 0 ms (100%), Space: 6.7 MB (9.27%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.c
@@ -0,0 +1,27 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int comb(int n, int k){
+    if (k == 0)
+        return 1;
+    if (k == 1)
+        return n;
+    long double res = 1;
+    int a = k;
+    while (n>k){
+        res *= (long double)n/(n-k);
+        --n;
+    }
+    return round(res);
+}
+
+int* getRow(int rowIndex, int* returnSize){
+    int *ans = malloc((1+rowIndex)*sizeof(*ans));
+    *returnSize = rowIndex+1;
+    for (int i = 0; i<=*returnSize/2; ++i){
+        int val = comb(rowIndex, i);
+        ans[i] = val;
+        ans[*returnSize-1-i] = val;
+    }   
+    return ans;
+}

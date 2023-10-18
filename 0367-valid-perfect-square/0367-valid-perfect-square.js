5c1aa4663b73e9eec50ba0546d37fc5364c3fc82
Tomas Achaval <134091945+achaval-tomas@users.noreply.github.com>
2023-10-18 19:54:55 -0300

    Time: 1880 ms (13.01%), Space: 42.2 MB (13.12%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+    if (num == 1){return true;}
+    for (let i = 0; i<=num/2; i++){
+        if (i*i == num){
+            return true;
+        }
+    }
+    return false;
+};

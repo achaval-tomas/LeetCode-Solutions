46877f595fae8801c0aad2abeece2e774029f614
Tomas Achaval <134091945+achaval-tomas@users.noreply.github.com>
2023-10-31 22:54:46 -0300

    Time: 6 ms (58.18%), Space: 6.5 MB (96.36%) - LeetHub

---
+++ 3183-find-the-k-or-of-an-array/3183-find-the-k-or-of-an-array.c
@@ -0,0 +1,18 @@
+int findKOr(int* nums, int numsSize, int k) {
+    long mask = 0x1;
+    int kOR = 0;
+    while( mask <= INT_MAX ){
+        int count = 0;
+        // Check for k numbers with the same bit set as mask.
+        for (int j = 0; j<numsSize && count<k; ++j)
+            count += (bool)(nums[j] & mask);
+
+        // If there were k numbers, add mask to the final answer.
+        if (count == k)
+            kOR += mask; 
+
+        // Shift Right mask to check the next bit.
+        mask = mask << 1;
+    }
+    return kOR;
+}

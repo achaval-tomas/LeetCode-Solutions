08621d795c7b99ac9c14088fff141d1065a18bb9
Tomas Achaval <134091945+achaval-tomas@users.noreply.github.com>
2023-11-09 18:24:01 -0300

    Time: 0 ms (100%), Space: 6.7 MB (63%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.c
+++ 0541-reverse-string-ii/0541-reverse-string-ii.c
@@ -2,7 +2,7 @@ char* reverseStr(char* s, int k) {
     int len = strlen(s);
     for (int i = 0; i<len; i += 2*k){
         if (i+k > len)
-            k = len-i;
+            k = len-i; // reverse last remaining chars
         for (int j = 0; j<k/2 && i+j<len; ++j){
             char aux = s[i+j];
             s[i+j] = s[i+k-j-1];

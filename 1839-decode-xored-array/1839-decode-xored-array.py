6387aed5e1ee2590b30db6733ef892e935c34253
Tomas Achaval <134091945+achaval-tomas@users.noreply.github.com>
2023-11-12 02:13:47 -0300

    Time: 173 ms (5.21%), Space: 15 MB (27.01%) - LeetHub

---
+++ 1839-decode-xored-array/1839-decode-xored-array.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def decode(self, encoded, first):
+        arr = []
+        arrlen = len(encoded)+1
+        arr.append(first)
+        for i in range(1, arrlen):
+            arr.append(arr[i-1] ^ encoded[i-1])
+        return arr
+        

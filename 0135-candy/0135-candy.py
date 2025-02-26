b8e332a55d5f67eb5b3ad1dbf93cc36193e4fd69
Tomas Achaval <134091945+achaval-tomas@users.noreply.github.com>
2025-02-26 19:31:15 -0300

    Time: 19 ms (73.2%), Space: 14.6 MB (74.89%) - LeetHub

---
+++ 0135-candy/0135-candy.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+        n = len(ratings)
+        candies = [1 for _ in range(n)]
+
+        for i in range(n-1):
+            if (i < n - 1 and ratings[i+1] > ratings[i]):
+                candies[i+1] = candies[i] + 1
+
+        for i in range(n-1, 0, -1):
+            if (i > 0 and ratings[i-1] > ratings[i] and candies[i-1] <= candies[i]):
+                candies[i-1] = candies[i] + 1
+
+        return sum(candies)

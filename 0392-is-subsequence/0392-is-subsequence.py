bab84c6cd79607fcf499d5319188b7e48cc36b9f
Tomas Achaval <134091945+achaval-tomas@users.noreply.github.com>
2025-02-28 00:55:04 -0300

    Time: 3 ms (37.24%), Space: 12.5 MB (70.31%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -9,6 +9,6 @@ class Solution(object):
         for c in t:
             if check < len(s) and c == s[check]:
                 check += 1
-            if check == len(s):
+            elif check == len(s):
                 return True
-        return False
+        return check == len(s)

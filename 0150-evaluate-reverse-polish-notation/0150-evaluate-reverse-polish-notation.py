b2c2cbf1e7e08b40a62c57bfe9033e86a59f9956
Tomas Achaval <134091945+achaval-tomas@users.noreply.github.com>
2025-03-07 03:27:56 -0300

    Time: 10 ms (46.11%), Space: 13.8 MB (30.82%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -4,18 +4,21 @@ class Solution(object):
         :type tokens: List[str]
         :rtype: int
         """
-        stack = []
-        i = 0
         n = len(tokens)
+        if n == 1:
+            return int(tokens[0])
+
         operators = {
             "+": lambda a, b: a + b,
             "-": lambda a, b: a - b,
             "*": lambda a, b: a * b,
             "/": lambda a, b: int(float(a) / b), # truncate towards zero
         }
+        stack = []
+        i = 0
         while i < n:
             num_count = 0
-            while tokens[i] not in operators:
+            while i < n and tokens[i] not in operators:
                 stack.append(int(tokens[i]))
                 i += 1
             b = stack.pop(-1)
@@ -23,6 +26,5 @@ class Solution(object):
             res = operators[tokens[i]](a, b)
             stack.append(res)
             i += 1
-            print(stack)
 
         return int(stack[0])
